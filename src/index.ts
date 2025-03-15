7b50efa33a34fc5689f3dc674562ac606932faf9
NadaMohamedShoukry <nadashoukry23@gmail.com>
2025-03-15 23:57:15 +0200

    Product Feature

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
-import userRoutes from "./routes/userRoutes"
+import userRoutes from "./routes/userRoutes";
+import productRoute from "./routes/productRoute"
+import { seedInitialProducts } from "./services/productService";
 const app = express();
 const port = 3001;
 //built in middleware to convert json format to be added in request.body
@@ -11,8 +13,11 @@ mongoose
   .then(() => console.log("Mongo Connected"))
   .catch((error)=> console.log("Failed to connect",error));
 
+//seed the products to database.
+seedInitialProducts();
 
 app.use('/user',userRoutes);
+app.use('/products',productRoute);
 
 app.listen(port,()=>{
     console.log(`Server is running at http://localhost:${port}`)
